5d7314efe6b029b81585e376a2b12239ed490185
Francois Berenger <unixjunkie@sdf.org>
2020-03-09 16:17:47 +0900

    test.r: example R script with many things working regargind PLS regression

--- test.r
+++ test.r
@@ -3,25 +3,38 @@ library("pls", quietly = TRUE)
 
 # FBR: use the cLogP dataset
 
+# load training set
 data <- as.matrix(read.table("data/Boston_regr_train.csv", colClasses = "numeric", header = TRUE))
 ydim <- dim(data)[1]
 xdim <- dim(data)[2]
 stopifnot(xdim == 14 && ydim == 456)
-xs <- data[, 2:14]
-ys <- data[, 1:1]
-mydata <- data.frame(y = ys, x = xs)
+xs <- data[, 2:14] # all lines, some columns
+ys <- data[, 1:1] # all lines, 1st column only as the response var.
+# I(x) prevents xs from having its columns split into different variables
+train_data <- data.frame(y = ys, x = I(xs))
 
 # just a train a model
-model <- plsr(data.y ~ data.x, ncomp = 10, method = "simpls", data = mydata, validation = "none")
+model <- plsr(y ~ x, ncomp = 10, method = "simpls", data = train_data, validation = "none")
 
-# FBR: find the best ncomp using NxCV - fix the number of folds !!!
+# load test set
+data <- as.matrix(read.table("data/Boston_regr_test.csv", colClasses = "numeric", header = TRUE))
+ydim <- dim(data)[1]
+xdim <- dim(data)[2]
+stopifnot(xdim == 14 && ydim == 51)
+xs_test <- data[, 2:14] # all lines, some columns
+ys_test <- data[, 1:1] # all lines, 1st column only as the response var.
+# I(x) prevents xs from having its columns split into different variables
+test_data <- data.frame(y = ys_test, x = I(xs_test))
 
-model <- plsr(y ~ x, method = "simpls", data = mydata, validation = "CV")
+# fix nfolds in NxCV
+# find the best ncomp value
+model <- plsr(y ~ x, method = "simpls", data = train_data, validation = "CV", segments = 5)
+r2 <- R2(model)
+plot(r2)
+r2s <- unlist(r2[1])
+ncomp_best <- which.max(r2s)
 
+# predict using trained model
+values <- predict(model, ncomp = ncomp_best, newdata = test_data)
 
-model <- plsr(octane ~ NIR, method = "simpls", data = gasTrain, validation = "CV")
-plot(RMSEP(model)) # FBR: extract ncomp from there; i.e. ncomp where CV is minimum
-
-predict(model, ncomp = 10, newdata = gasTest)
-
-predplot(model, ncomp = 10, newdata = gasTest, asp = 1, line = TRUE)
+predplot(model, ncomp = ncomp_best, newdata = test_data, asp = 1, line = TRUE)
