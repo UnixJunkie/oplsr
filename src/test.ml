57a74659c169e3681cd65da3d53bd06e762f547e
Francois Berenger <unixjunkie@sdf.org>
2020-03-10 15:00:43 +0900

    rewrote the test exe

--- src/test.ml
+++ src/test.ml
@@ -1,132 +1,29 @@
+
 open Printf
 
 module CLI = Minicli.CLI
 module L = BatList
 module Log = Dolog.Log
-
-module Score_label = struct
-  type t = bool * float (* (label, pred_score) *)
-  let get_label (l, _) = l
-  let get_score (_, s) = s
-end
-
-module ROC = Cpm.MakeROC.Make(Score_label)
+module PLS = Oplsr.PLS
 
 let main () =
   Log.(set_log_level DEBUG);
   Log.color_on ();
   let _argc, args = CLI.init () in
   let verbose = CLI.get_set_bool ["-v"] args in
-  let nb_trees = 500 in
-  (* classification test *)
-  assert(Oranger.RF.(train ~debug:verbose
-                       Classification
-                       nb_trees
-                       "data/train.txt"
-                       "1832"
-                       "ranger_class_model.rf"));
-  let preds =
-    Oranger.RF.predict ~debug:verbose
-      nb_trees
-      "data/test.txt"
-      "ranger_class_model.rf" in
-  match preds with
-  | None -> assert(false)
-  | Some score_stddevs ->
-    L.iter (fun (mean, stddev) ->
-        Log.info "%f %f" mean stddev
-      ) score_stddevs;
-    let labels = [true; true; true; true; true;
-                  false; false; false; false; false] in
-    let scores = L.map fst score_stddevs in
-    let score_labels = L.combine labels scores in
-    let auc = ROC.auc score_labels in
-    Log.info "AUC: %.3f\n" auc;
-    (* regression test *)
-    assert(Oranger.RF.(train ~debug:verbose
-                         Regression
-                         nb_trees
-                         "data/Boston_regr_train.csv"
-                         "14"
-                         "ranger_regr_model.rf"));
-    let preds =
-      Oranger.RF.predict ~debug:verbose
-        nb_trees
-        "data/Boston_regr_test.csv"
-        "ranger_regr_model.rf" in
-    match preds with
-    | None -> assert(false)
-    | Some score_stddevs ->
-      let actual =
-        [22.6;
-         16.0;
-         17.2;
-         20.9;
-         21.7;
-         32.5;
-         37.6;
-         43.5;
-         13.4;
-         18.9;
-         20.4;
-         11.7;
-         13.8;
-         22.0;
-         36.4;
-         10.9;
-         22.0;
-         14.4;
-         18.1;
-         12.6;
-         26.4;
-         32.4;
-         15.2;
-         11.0;
-         18.9;
-         19.0;
-         21.2;
-         33.2;
-         50.0;
-         35.4;
-         20.3;
-         23.9;
-         20.0;
-         19.9;
-         24.0;
-         23.9;
-         21.4;
-         36.5;
-         22.4;
-         24.5;
-         33.2;
-         16.5;
-         32.7;
-         18.2;
-         21.4;
-         37.3;
-         26.6;
-         20.6;
-         50.0;
-         22.0]
-        in
-      begin
-        L.iter (fun (mean, stddev) ->
-            Log.info "%f %f" mean stddev
-          ) score_stddevs;
-        Oranger.Utls.with_out_file "toplot" (fun out ->
-            L.iter (fun (x, y) ->
-                fprintf out "%f %f\n" x y
-              ) (L.combine actual (L.map fst (BatOption.get preds)))
-          );
-        let _ret = Sys.command "\
-gnuplot --persist <<EOF\n\
-set xlabel 'actual'\n\
-set ylabel 'predicted'\n\
-f(x) = a*x + b\n\
-fit f(x) 'toplot' u 1:2 via a,b\n\
-set key left\n\
-plot 'toplot' u 1:2 t 'preds', f(x) t 'linear fit'\n\
-EOF\n" in ()
-      end
+  let train_fn = CLI.get_string ["--train"] args in
+  let test_fn = CLI.get_string ["--test"] args in
+  let nb_features = CLI.get_int ["-n"] args in
+  let nfolds = CLI.get_int_def ["--NxCV"] args 10 in
+  let ncomp_best, val_R2 = PLS.optimize verbose nb_features train_fn nfolds in
+  Log.info "ncomp_best: %d trainR2: %f" ncomp_best val_R2;
+  let model_fn = PLS.train verbose nb_features train_fn ncomp_best in
+  let preds = PLS.predict verbose ncomp_best model_fn nb_features test_fn in
+  let actual_fn = Filename.temp_file "PLS_test_" ".txt" in
+  ignore(Sys.command (sprintf "cut -d' ' -f1 %s > %s" test_fn actual_fn));
+  let actual = Oplsr.Utls.float_list_of_file actual_fn in
+  Sys.remove actual_fn;
+  let test_R2 = Cpm.RegrStats.r2 actual preds in
+  Log.info "testR2: %f" test_R2
 
 let () = main ()
