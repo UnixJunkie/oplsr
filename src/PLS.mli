11d4d3612e7a8a8eaf007a294778f28102f251bb
Francois Berenger <unixjunkie@sdf.org>
2020-04-06 15:28:05 +0900

    PLS: polished the ocamldoc

--- src/PLS.mli
+++ src/PLS.mli
@@ -12,20 +12,23 @@
     as well as the corresponding R^2 (a model regression performance metric
     in [0:1]; near zero is dangerous, near one is good).
     [debug] is a verbosity flag.
-    [nb_features] is the number of features of each data item.
-    [train_csv_fn] is the name of the CSV file holding your data.
+    [nb_features] is the number of features in each data item.
+    [train_csv_fn] is the name of the CSV file holding your training data.
     This file must have the value to model in the first column (Y),
     all other columns are feature values (X_i).
     This file must be in space-separated dense format.
     Its first line is a CSV header (column numbers are fine).
-    [nb_folds] is the number of folds of cross validation;
+    [nb_folds] is the number of folds of cross validation used during training;
     five or ten is standard for this one. *)
-val Oplsr.PLS.optimize bool -> int -> string -> int -> int * float
+val optimize: bool -> int -> string -> int -> int * float
 
 (** [trained_model_fn = train debug nb_features train_csv_fn nb_comp_opt]
-    train a model using the given (optimal) number of components.
-    The filename where the model is stored is returned. *)
-val Oplsr.PLS.train bool -> int -> string -> int -> string
+    train a model using the given (optimal) number of components [nb_comp_opt]
+    and training set CSV file [train_csv_fn].
+    Each data item must have [nb_features] number of features.
+    The filename where the model is stored is returned.
+    [debug] is a verbosity flag. *)
+val train: bool -> int -> string -> int -> string
 
 (** [predictions = \
     predict debug nb_comp_opt trained_model_fn nb_features test_csv_fn]
@@ -33,5 +36,6 @@ val Oplsr.PLS.train bool -> int -> string -> int -> string
     given the optimal number of components [nb_comp_opt] and
     the number of features in each data element [nb_features], reading
     data to predict from [test_csv_fn].
+    [debug] is a verbosity flag.
     The raw list of predicted values is returned. *)
-val Oplsr.PLS.predict bool -> int -> string -> int -> string -> float list
+val predict: bool -> int -> string -> int -> string -> float list
